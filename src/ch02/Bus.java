7414d8f7e773e50a7eddd86ab1f0a8572ad24ba4
yj <bit-user@bit>
2016-09-12 15:57:59 +0900

    ch02

---
+++ src/ch02/Bus.java
@@ -0,0 +1,9 @@
+package ch02;
+
+public class Bus extends Car{
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/ch02/Car.java
@@ -0,0 +1,10 @@
+package ch02;
+
+public class Car {
+	private int speed;
+	protected int accelator = 10;
+	
+	public void drive(){
+		speed = 0;
+	}
+}
