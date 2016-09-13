e52dd46f125dc0a02f9e22407bbdee87a539f720
yj <bit-user@bit>
2016-09-13 10:52:52 +0900

    외부패키지 접근권한

---
+++ src/bit2016/goodsman/db/CusteomerVVIP.java
@@ -0,0 +1,11 @@
+package bit2016.goodsman.db;
+
+import bit2016.goodsman.data.Customer;
+
+public class CusteomerVVIP extends Customer {
+	void set(){
+		name = "";
+		phone = "";
+//		email = ""; private만 접근불가
+	}
+}
