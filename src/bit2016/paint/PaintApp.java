ee94c394bea22f6d38422c51d9fe6227f9d5959c
yj <bit-user@bit>
2016-09-20 14:22:35 +0900

    추상클래스 적용

--- src/bit2016/paint/PaintApp.java
+++ src/bit2016/paint/PaintApp.java
@@ -12,8 +12,26 @@ public class PaintApp {
 
 		Point point2 = new Point(200, 200);
 		point2.show(false);
-		
+
 		Point point3 = new ColorPoint(50, 50, "노란색");
 		point3.show();
+		point3.show(false);
+		point3.show(true);
+
+		Shape rectnagle = new Rectangle();
+		drow(rectnagle);
+
+		Shape circle = new Circle();
+		drow(circle);
+
+		Shape triangle = new Triangle();
+		drow(triangle);
+
+		drow(new Pentagon());
+
+	}
+
+	public static void drow(Shape shape) {
+		shape.drow();
 	}
 }
---
+++ src/bit2016/paint/Pentagon.java
@@ -0,0 +1,10 @@
+package bit2016.paint;
+
+public class Pentagon extends Shape {
+
+	@Override
+	public void drow() {//추상메서드를 상속받으면 오버라이드 반드시 입력
+		System.out.println("오각형을 그렸습니다.");
+	}
+
+}
